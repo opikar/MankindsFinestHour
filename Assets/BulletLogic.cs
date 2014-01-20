340e9d9f5f418047c0b5dc4e7196d3a48c32d795
jesseve <jesseverainen@gmail.com>
2014-01-20 11:44:25 +0200

    weapon

---
+++ Assets/BulletLogic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletLogic : MonoBehaviour {
+
+	float speed = 100f;
+	Vector2 velocity;
+	// Use this for initialization
+	void Start () {
+		//set speed for the bullet when it's instantiated
+		velocity = new Vector2(transform.right.x, transform.right.y) * speed;
+		rigidbody2D.AddForce(velocity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if the bullet goes out of screen, destroy it
+		if(!gameObject.renderer.isVisible)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform bulletSpawn;
+	GameObject clone;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Z))
+		{
+			clone = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+		}
+	}
+}

e8f867cfd7dcfbcca1fc57ea54e976f8df1205a2
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-01-24 15:01:54 +0200

    layercollision stuff from old playermovement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,25 +8,45 @@ public class Movement : MonoBehaviour {
 
 	bool facingRight;
 
-	public Transform groundCheck;
+	Transform groundCheck;
+	LayerMask groundMask;
+	LayerMask playerMask;
 	public LayerMask whatIsGround;
 	float groundRadius = 0.2f;
 	bool grounded = false;
 	bool doubleJump = false;
+	RaycastHit2D hit;
 	// Use this for initialization
 	void Start () {
-	
+		groundCheck = transform.Find ("groundCheck");
+		groundMask = LayerMask.NameToLayer("Ground");
+		playerMask = LayerMask.NameToLayer("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		hit = Physics2D.Linecast(transform.position, groundCheck.position, 1 << groundMask);
+		//Check if we are inside a collider
+		if(hit.fraction == 0)
+			grounded = false;
+		else
+			grounded = true;
+
+		if(grounded) {
+			Physics2D.IgnoreLayerCollision(groundMask, playerMask, false);
+
+		}
+		else {
+			//Disable collisions while we are in air
+			Physics2D.IgnoreLayerCollision(groundMask, playerMask, true);
+		}
 	}
+
 	public void Move(float direction){
 		rigidbody2D.velocity = new Vector2(speed * direction, rigidbody2D.velocity.y);
 	}
 	public void Jump(){
-		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		//grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
 		if(grounded || !doubleJump){
 			rigidbody2D.AddForce(Vector2.up * jumpForce);
 			if(!grounded)

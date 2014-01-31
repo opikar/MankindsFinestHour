68d52bd981c44d6fe425200e12bc440b0e0b9dd0
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-01-31 11:53:32 +0200

    small improvement to camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public float CameraSpeed = 1.2f;
+	public GameObject[] waypoints;
+
+	int nextWaypoint = -1;
+	float cameraZ;
+
+	// Use this for initialization
+	void Start () {
+		cameraZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(waypoints.Length > nextWaypoint || nextWaypoint == -1)
+			return;
+		if (transform.position.x < waypoints[nextWaypoint].transform.position.x) 
+		{
+			Vector3 movement = Vector3.MoveTowards(transform.position, waypoints[nextWaypoint].transform.position, CameraSpeed * Time.deltaTime);
+			movement.z = cameraZ;
+			transform.position = movement;
+		}
+	}
+
+	public void MoveToNext() {
+		nextWaypoint++;
+	}
+}

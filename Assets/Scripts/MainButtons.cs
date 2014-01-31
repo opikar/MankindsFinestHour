6c7319d8e5cdf787cf58a2c2ffb172617c99c0c3
Pasketi <pasketi@gmail.com>
2014-01-31 15:15:14 +0200

    Added MainMenu, some placeholder graphics and possible Loading screen. PS. Never apply Epilepsy.cs to a camera

---
+++ Assets/Scripts/MainButtons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0) && guiTexture.HitTest(Input.mousePosition))
+		{
+			if(guiTexture.name == "New")
+			{
+				Application.LoadLevel("Level1");
+				//Debug.Log("New");
+			}
+			else if(guiTexture.name == "Load")
+			{
+				Debug.Log("Load");
+			}
+			else if(guiTexture.name == "Options")
+			{
+				Debug.Log("Options");
+			}
+			else if(guiTexture.name == "Quit")
+			{
+				Debug.Log("Quit");
+				Application.Quit();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/State-Machine.cs
@@ -0,0 +1,18 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+public class State-Machine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//if (Application.isLoadingLevel == true && Application.loadedLevelName != "Loading") Application.LoadLevel("Loading");
+	//Application.LoadLevel("LevelName");
+	//Application.LoadLevel("LevelIndex");
+}*/

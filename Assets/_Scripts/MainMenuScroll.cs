afa21d74685e5c1532f63c32cf9f5ba8ad93aa8a
Pasketi <pasketi@gmail.com>
2014-02-03 13:46:11 +0200

    Adding placeholders and modifying the Main Menu

---
+++ Assets/_Scripts/MainMenuScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScroll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x <= -18.8f)
+		{
+			transform.position = new Vector3(18.8f, 1, 0);
+		}
+		transform.position -= new Vector3(0.2f, 0, 0) * Time.deltaTime;
+	}
+}

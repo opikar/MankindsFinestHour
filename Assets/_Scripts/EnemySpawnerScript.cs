95797e5987e48a4fef5cab61c24fa3bd11ab9a98
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-02-03 11:30:23 +0200

    Spawn enemies

---
+++ Assets/_Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnerScript : MonoBehaviour {
+	public GameObject enemyType;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Instantiate(enemyType, transform.position, transform.rotation);
+		Destroy (this);
+	}
+}

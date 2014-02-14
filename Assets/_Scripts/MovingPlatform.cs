f28960223d6cd8af4cbdc382160e15a558b18985
jesseve <jesseverainen@gmail.com>
2014-02-14 23:13:29 +0200

    moving platform. needs a little sharpening still

--- Assets/_Scripts/MovingPlatform.cs
+++ Assets/_Scripts/MovingPlatform.cs
@@ -1,10 +1,88 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MovingPlatform : MonoBehaviour {
 
+	public GameObject start;
+
+	private int i_index;
+	private Transform m_transform;
+	private float f_range = 5f;
+	private Vector3 v_moveDirection;
+	private List<Transform> waypoint;
+
+	void Start()
+	{
+		m_transform = transform;
+		waypoint = GetPath();
+	}
+
 	void Update()
 	{
+		if ((m_transform.position - waypoint[i_index].position).sqrMagnitude > f_range)
+		{
+			v_moveDirection = waypoint [i_index].position - m_transform.position;
+			v_moveDirection.Normalize();
+		}
+		else NextIndex();
 
+		m_transform.Translate(v_moveDirection * Time.deltaTime * 20f);
 	}
+
+	void NextIndex()
+	{
+		if (++i_index >= waypoint.Count) i_index = 0;
+	}
+
+	List<Transform> GetPath()
+	{
+		string str = null;
+		int index=0;
+		str = start.tag;
+		Transform[] t = GetComponentsInChildren<Transform>();
+		List<GameObject> gos = new List<GameObject>();
+		for(int i = 0; i < t.Length; i++)
+		{
+			if(t[i].tag == "PlatformWaypoint")
+				gos.Add(t[i].gameObject);
+			print (t[i].position);
+		}
+
+		List<Transform> waypoints = new List<Transform>();
+		waypoints.Add (start.transform);
+		start.GetComponent<PlatformWaypointScript>().SetUsed(true);
+		while(true){
+			Transform closest = FindClosest(waypoints[index],gos);
+			if(closest != null)
+				waypoints.Add (closest);
+			if(++index >= gos.Count)break;
+		}
+		return waypoints;
+	}
+
+	Transform FindClosest(Transform start, List<GameObject> obj){
+		Transform closest = null;
+		float distance = Mathf.Infinity;
+		foreach(GameObject go in obj){
+			PlatformWaypointScript sc = go.GetComponent<PlatformWaypointScript>();
+			if(start!=go.transform && !sc.GetUsed()){
+				//if(!Physics.Linecast (start.position,go.transform.position,layerMask)){
+				float dist = (start.position - go.transform.position).magnitude;
+				if(dist < distance){
+					distance = dist;
+					closest = go.transform;
+				}
+				//}
+			}
+		}
+		if(closest != null){
+			closest.gameObject.GetComponent<PlatformWaypointScript>().SetUsed (true);
+		}
+		return closest;
+	}
+
+
+
+
 }
---
+++ Assets/_Scripts/PlatformWaypointScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformWaypointScript : MonoBehaviour {
+
+	private Vector3 position;
+	bool used;
+
+	void Awake()
+	{
+		position = transform.position;
+	}
+
+	void Update()
+	{
+		transform.position = position;
+	}
+
+	public bool GetUsed(){
+		return used;
+	}
+	public void SetUsed(bool b){
+		used = b;
+	}
+	public float radius = 0.5f;
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(transform.position, radius);
+	}
+}

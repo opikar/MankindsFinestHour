25f2c484ec030f33efa3b6531cd073b7d1bb9332
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-02-24 12:42:02 +0200

    Fix bug whre selected enemy type was reset.

--- Assets/Editor/SpawnScriptEditor.cs
+++ Assets/Editor/SpawnScriptEditor.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 [CustomEditor(typeof(SpawnerScript))]
 public class SpawnScriptEditor : Editor {
-	int index = 0;
+	int index;
 	string[] keys;
 
 	public void OnEnable() {
 		keys = GameState.GetKeys();
+		index = FindSelectedIndex ();
 	}
 
 	public override void OnInspectorGUI ()
@@ -20,5 +21,15 @@ public class SpawnScriptEditor : Editor {
 		tmp.choice = keys[index];
 
 		EditorUtility.SetDirty(target);
+	}
+
+
+	int FindSelectedIndex() {
+		SpawnerScript tmp = target as SpawnerScript;
+		for(int i = 0; i < keys.Length; i++) {
+			if(keys[i] == tmp.choice)
+				return i;
+		}
+		return 0;
 	}
 }

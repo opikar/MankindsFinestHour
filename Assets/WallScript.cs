3892b599a83b4168d83cb3d5ac2be9efd71c1b26
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-03-07 12:34:20 +0200

    Camera walls

---
+++ Assets/WallScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+	Transform leftWall, rightWall;
+
+	// Use this for initialization
+	void Start () {
+		leftWall = GameObject.Find("CameraWallLeft").transform;
+		rightWall = GameObject.Find("CameraWallRight").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 right = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, 0));
+		Vector3 left = Camera.main.ScreenToWorldPoint (new Vector3(0, 0));
+
+		right.y = left.y = transform.position.y;
+
+		rightWall.position = right;
+		leftWall.position = left;
+	}
+}

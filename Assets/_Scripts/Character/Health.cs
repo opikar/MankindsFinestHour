c2693c939486cc4d6e9c156f53b0d10ee2bc2916
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-03-07 16:00:48 +0200

    Health-bar for testing, but I kinda like it

--- Assets/_Scripts/Character/Health.cs
+++ Assets/_Scripts/Character/Health.cs
@@ -7,14 +7,35 @@ public class Health : MonoBehaviour
     public float fullHP = 10f;
 	
     public float f_currentHP;
+	public Rect healthBar = new Rect(40, 25, 200, 30);
+
+	private Rect currentHealthBar;
 	private Character m_character;
+	private Texture2D healthTexture;
+	private Texture2D barTexture;
     #endregion
 
     #region UNITY_METHODS
-    void Start () 
+    void Start ()
     {
 		m_character = GetComponent<Character>();
 		f_currentHP = fullHP;
+
+		healthTexture = new Texture2D(1, 1);
+		healthTexture.SetPixel(0, 0, Color.green);
+		barTexture = new Texture2D(1, 1);
+		barTexture.SetPixel(0, 0, Color.red);
+		healthTexture.Apply();
+		barTexture.Apply();
+		currentHealthBar = healthBar;
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(healthBar, barTexture);
+		currentHealthBar.width = f_currentHP/fullHP*healthBar.width;
+		GUI.DrawTexture(currentHealthBar, healthTexture);
+
 	}
     #endregion
 

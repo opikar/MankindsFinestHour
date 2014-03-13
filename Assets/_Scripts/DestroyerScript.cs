9f409be4ec28b4b469bbcc157b074c1685865ecc
jesseve <jesseverainen@gmail.com>
2014-03-13 14:45:16 +0200

    Destroyer

---
+++ Assets/_Scripts/DestroyerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyerScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			PlayerManager manager = other.gameObject.GetComponent<PlayerManager> ();
+			if (manager != null)
+				manager.Die ();
+		} 
+		else if (other.tag == "Enemy") 
+		{
+			EnemyManager manager = other.gameObject.GetComponent<EnemyManager> ();
+			if (manager != null)
+				manager.Die ();
+		} 
+	}
+}

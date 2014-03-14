b938be03966773ac9b5f10a0feece93f7ff3f7a3
jesseve <jesseverainen@gmail.com>
2014-03-14 15:21:31 +0200

    save class

---
+++ Assets/_Scripts/SaveScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Save()
+	{}
+	void Load()
+	{}
+}

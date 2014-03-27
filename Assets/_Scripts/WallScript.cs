ae5d04f3d0572e33e0d03e0b53f312dddfe1dbe7
Erkki Parkkulainen <erkki.parkkulainen@gmail.com>
2014-03-27 13:05:55 +0200

    Scale spawner according to screen

--- Assets/_Scripts/WallScript.cs
+++ Assets/_Scripts/WallScript.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class WallScript : MonoBehaviour {
 	Transform leftWall, rightWall;
 
-	// Use this for initialization
-	void Start () {
+	void Awake() {
 		leftWall = transform.Find("CameraWallLeft");
 		rightWall = transform.Find("CameraWallRight");
+
+		MoveWalls ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		MoveWalls();
+	}
+
+	void MoveWalls() {
 		Vector3 right = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, 0));
 		Vector3 left = Camera.main.ScreenToWorldPoint (new Vector3(0, 0));
-
+		
 		right.y = left.y = transform.position.y;
-
+		
 		rightWall.position = right;
 		leftWall.position = left;
 	}

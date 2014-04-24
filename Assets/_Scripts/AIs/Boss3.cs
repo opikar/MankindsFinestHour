2c493a1432effce5babf1af9453943d5eb5cb49f
jesseve <jesseverainen@gmail.com>
2014-04-24 16:01:32 +0300

    boss levels 3 and 5

---
+++ Assets/_Scripts/AIs/Boss3.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss3 : ScriptedEnemy {
+
+
+	override protected void Start () {
+		base.Start ();
+		Action action1 = JumpAction;
+		Action action2 = ShootRapidlyAction;
+		Action action3 = DropAction;
+        Action action4 = ShootAction;
+        Action action5 = ShootBigBullet;
+
+		currentState.AddAction(action1);
+		currentState.AddAction(action2);
+		currentState.AddAction(action3);
+		currentState.AddAction(action4);
+        currentState.AddAction(action5);
+	}
+
+    public override void Stomped()
+    {
+        print("boss3");
+    }
+}

79329ec33b24dad99e4c9a0df4de508ea9370ccb
jesseve <jesseverainen@gmail.com>
2014-04-25 12:32:35 +0300

    boss stuff

--- Assets/_Scripts/AIs/Scripted/ScriptedEnemy.cs
+++ Assets/_Scripts/AIs/Scripted/ScriptedEnemy.cs
@@ -13,9 +13,11 @@ public abstract class ScriptedEnemy : EnemyManager {
     public float bulletPerSecond;
     public float waitAfterAction = 1.5f;
     public float collisionDamage = 20f;
+    public float startRage = 0.5f;
 
     private float maxY, minY, maxX, minX;
     protected Transform player;
+    protected bool rage;
     protected float timer;
     protected Action lastAction;
     private Vector3 platformPosition, platformScale;
@@ -80,6 +82,8 @@ public abstract class ScriptedEnemy : EnemyManager {
 		if(!actionRunning) {
 			StartCoroutine(RunAction ());
 		}
+        if (health.f_currentHP < GetMaxHealth() * .5f)
+            Rage();
         FlipBoss();
 	}
 
@@ -107,6 +111,10 @@ public abstract class ScriptedEnemy : EnemyManager {
         print("Boss died");
     }
 
+    protected virtual void Rage()
+    {
+        rage = true;
+    }
 
     //ACTIONS FOR ALL THE BOSSES
 
@@ -174,7 +182,7 @@ public abstract class ScriptedEnemy : EnemyManager {
         }
     }
 
-    protected IEnumerator ShootRapidlyAction()
+    protected virtual IEnumerator ShootRapidlyAction()
     {
         if (GetGrounded())
         {

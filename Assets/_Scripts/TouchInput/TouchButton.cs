037177969427ac7831b09bde9319d6499fc2d0fd
jesseve <jesseverainen@gmail.com>
2014-04-30 20:44:25 +0300

    button for touch devices

---
+++ Assets/_Scripts/TouchInput/TouchButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchButton : MonoBehaviour {
+
+    Rect area;
+
+	// Use this for initialization
+	void Start () {
+        area = guiTexture.pixelInset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < Input.touchCount; i++)
+        { 
+            if(Input.touches[i].phase == TouchPhase.Began && area.Contains(Input.touches[i].position))
+                Debug.Log("touched button");
+        }
+	}
+}

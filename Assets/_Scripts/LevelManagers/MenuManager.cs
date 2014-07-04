f4428e01c7b9cbd340c5c7487dff482eff634bd2
jesseve <jesseverainen@gmail.com>
2014-07-04 20:09:15 +0300

    Fixed the pause handling
    Player is now not able to pause game in main menu or level select

---
+++ Assets/_Scripts/LevelManagers/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public State startState;
+
+	// Use this for initialization
+	void Start () {
+        GameManager.instance.SetState(startState);
+	}
+	
+}

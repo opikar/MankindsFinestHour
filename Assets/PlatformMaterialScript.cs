c8981da0528fb669fd55e670e12fe3ffcc568cea
Pasketi <pasketi@gmail.com>
2014-08-21 10:57:09 +0300

    Comments on material colouring. Coloring. Whatever.

--- Assets/PlatformMaterialScript.cs
+++ Assets/PlatformMaterialScript.cs
@@ -4,6 +4,19 @@ using System.Collections;
 public class PlatformMaterialScript : MonoBehaviour {
 
     private Material m;
+    /*
+
+	What is levelName?
+	What is a color bound to levelName?
+	Set the color to be LevelColor
+
+	private Color LevelColor = new Color(0F,0F,0F,1F);
+
+	After saying "Color" too many times, "Color" loses it's meaning.
+	Color makes you start to question yourself about Color.
+	Does "color" look weird to you now?
+
+    */
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +25,7 @@ public class PlatformMaterialScript : MonoBehaviour {
         x = transform.localScale.x;
         y = transform.localScale.y;
         m.SetTextureScale("_MainTex", new Vector2(x, y));
+        //m.color = LevelColor;
 	}
 	
 	// Update is called once per frame
